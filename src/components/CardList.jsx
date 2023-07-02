00c0299722c284d3ce0e08ae839cb2f61786d404
Jun Young <50202150+wsdx123@users.noreply.github.com>
2023-07-02 17:12:17 +0900

    chore: fix

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -36,53 +36,7 @@ function CardList() {
     <div>
       {posts.map((post) => {
         // 불리안 값으로 좋아요 하트 상태 변경
-
-
         return <CardItem key={`cardKey__${post.postId}`} post={post} />
-
-//         return (
-//           <div key={post.postId}>
-//             <img alt='postimage' src={post.imageUrl} style={{ width: '200px' }} /> <br />
-//             <div>
-//               <li style={{ display: 'flex' }}>
-//                 <ul>user.id</ul>
-//                 <ul>{post.period}</ul>
-//                 <ul>{post.destination}</ul>
-//                 <ul>{post.partner}</ul>
-//                 <ul>{post.content}</ul>
-//               </li>
-//             </div>
-//             {/* 좋아요: {post.isLiked.toString()} */}
-//             {/* postId: {post.postId} */}
-//             <div>
-//               <Link to={`/postPage/${post.postId}`}>EDIT</Link>
-//             </div>
-//             <div>
-//               <button
-//                 onClick={() => {
-//                   dispatch({
-//                     type: 'DELETE_POST',
-//                     payload: post.postId,
-//                   })
-//                 }}
-//               >
-//                 DELETE
-//               </button>
-
-//               <button
-//                 onClick={() => {
-//                   dispatch({
-//                     type: 'ISLIKED_POST',
-//                     payload: post.postId,
-//                   })
-//                 }}
-//               >
-//                 {heart}
-//               </button>
-//             </div>
-//           </div>
-//         )
-
       })}
     </div>
   )
